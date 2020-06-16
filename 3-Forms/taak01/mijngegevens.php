acb23e4a9af3cf162a115f9bb784fcde05db1cf6
jochem <jochem.gravendeel@student.rocva.nl>
2020-06-16 23:16:59 +0200

    Forms 1 af

---
+++ 3-Forms/taak01/mijngegevens.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    echo "Voornaam is ".$_POST["naam"]."<br>";
+    echo "Achternaam is ".$_POST["achternaam"]."<br>";
+    echo "Klas is ".$_POST["klas"]."<br>";
+    echo "Leeftijd is ".$_POST["leeftijd"]."<br>";
+    echo "Adres is ".$_POST["adres"]."<br>";
+    echo "Plaatsnaam is ".$_POST["plaatsnaam"]."<br>";
+    echo "Favoriete muziekband is ".$_POST["favorieteMuziekband"]."<br>";
+    ?>
+</body>
+</html>
