557f0c0b3ffeb64e363a565eafb790c5eebebb8e
jochem <jochem.gravendeel@student.rocva.nl>
2020-06-16 11:54:39 +0200

    af

---
+++ 1-Arrays/taak01/index.php
@@ -0,0 +1,7 @@
+<?php
+$voornaam = array("Mohammed" => "Mohammed");
+$achternaam = array("Mohammed" => "Ali");
+$leeftijd = array("Mohammed" => 17);
+$klas = array("Mohammed" => "9G");
+
+echo $voornaam["Mohammed"]." ".$achternaam["Mohammed"]." ".$leeftijd["Mohammed"]." ".$klas["Mohammed"];
