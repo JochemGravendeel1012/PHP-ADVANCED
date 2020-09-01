c2b954dcffca50aaa411def182eb224ac5f949aa
Jochem Gravendeel <jochemgravendeel@gmail.com>
2020-09-01 10:06:22 +0200

    1

--- 2-Loops/taak01/loops-while.php
+++ 2-Loops/taak01/loops-while.php
@@ -2,10 +2,10 @@
 $leeftijd = 0;
 
 while ($leeftijd <= 17) {
-    echo "Ik ben $leeftijd, dus ik mag nog niet stemmen. <br>";
+    echo " <ul> Ik ben $leeftijd, dus ik mag nog niet stemmen. </ul>";
     $leeftijd++;
 }
 
 if ($leeftijd >= 18) {
-    echo "Ik ben $leeftijd, dus ik heb stemrecht.";
+    echo " <ul> Ik ben $leeftijd, dus ik heb stemrecht. </ul>";
 }
